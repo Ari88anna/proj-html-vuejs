355752e8e21d51d27b01b07b4968c3c35825e940
Arianna Carotti <arianna.carotti@gmail.com>
2021-05-12 17:20:45 +0200

    popolato top-menu con Vue

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,25 @@
   \********************/
 /***/ (() => {
 
-
+var app = new Vue({
+  el: '#root',
+  data: {
+    topMenu: ['Home', 'Pages', 'Course Format', 'Courses', 'Demos'],
+    socialList: [{
+      name: '-facebook'
+    }, {
+      name: '-twitter'
+    }, {
+      name: '-instagram'
+    }, {
+      name: '-linkedin'
+    }, {
+      name: '-google-plus'
+    }, {
+      name: '-youtube'
+    }]
+  }
+});
 
 /***/ }),
 
---
+++ src/app.js
@@ -0,0 +1,29 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            topMenu: ['Home', 'Pages', 'Course Format', 'Courses', 'Demos' ],
+            socialList: [
+                {
+                    name: '-facebook'
+                },
+                {
+                    name: '-twitter'
+                },
+                {
+                    name: '-instagram'
+                },
+                {
+                    name: '-linkedin'
+                },
+                {
+                    name: '-google-plus'
+                },
+                {
+                    name: '-youtube'
+                }
+            ]
+
+        }
+
+})
