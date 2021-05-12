127baa7c4882551f6d554a9af9b6450ff699c7f9
Arianna Carotti <arianna.carotti@gmail.com>
2021-05-12 20:00:29 +0200

    popolata lista social nel footer con vue

--- src/app.js
+++ src/app.js
@@ -22,6 +22,42 @@ var app = new Vue(
                 {
                     name: '-youtube'
                 }
+            ],
+
+            socialListFoot: [
+                {
+                    name: '-facebook',
+                    bg: '#3b5998'
+                },
+                {
+                    name: '-instagram',
+                    bg: '#c32aa3'
+                },
+                {
+                    name: '-linkedin',
+                    bg: '#0077b5'
+                },
+                {
+                    name: '-pinterest',
+                    bg: '#bd081c'
+                },
+                {
+                    name: '-twitter',
+                    bg: '#1da1f2'
+                },
+                {
+                    name: '-instagram',
+                    bg: '#ea4c89'
+                },
+                {
+                    name: '-google-plus',
+                    bg: '#db4437'
+                },
+                {
+                    name: '-skype',
+                    bg: '#00aff0'
+                },
+                
             ]
 
         }
